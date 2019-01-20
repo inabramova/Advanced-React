928b38371fb8f905f115054253b53e895d0781d4
Irina Abramova <inabramova@gmail.com>
2019-01-19 19:02:34 -0800

    before switching over to frontend again.
    define couple basic mutations/queries

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){
+        //TODO: check if they are logged in
+        const item= await ctx.db.mutation.createItem({
+            data:{ ...args }
+        },info);
+        console.log(item);
+        return item;
+    }
+    // createDog(parent, args, ctx, info){
+    //     global.dogs=global.dogs||[];
+    //     const newDog={name: args.name};
+    //     global.dogs.push(newDog);
+    //     console.log(args);
+    //     return newDog;
+    // }
+};
 
 module.exports = Mutations;
