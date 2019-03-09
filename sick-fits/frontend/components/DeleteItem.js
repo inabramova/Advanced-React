46cafed389ee405776fa5153186f210f99639856
Irina Abramova <inabramova@gmail.com>
2019-03-09 10:32:30 -0800

    linter

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import {Mutation} from 'react-apollo';
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
-  mutation DELETE_ITEM_MUTATION($id: ID!){
-    deleteItem(id: $id){
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
       id
     }
   }
@@ -13,24 +13,33 @@ const DELETE_ITEM_MUTATION = gql`
 
 class DeleteItem extends Component {
   update = (cache, payload) => {
-    const data = cache.readQuery({query:ALL_ITEMS_QUERY});
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
     console.log(data.items);
-    data.items=data.items.filter(item=>item.id!==payload.data.deleteItem.id);
-    cache.writeQuery({query:ALL_ITEMS_QUERY,data});
-  }
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
-        variables={{id: this.props.id}}
+        variables={{ id: this.props.id }}
         update={this.update}
-        >
-        {(deleteItem, {error}) => (
-        <button onClick={()=>{ if (confirm("are you sure?")) deleteItem(); }}>
-          {this.props.children}</button>
+      >
+        {(deleteItem, { error }) => (
+          <button
+            type="button"
+            onClick={() => {
+              if (confirm('are you sure?')) deleteItem().catch;
+            }}
+          >
+            {this.props.children}
+          </button>
         )}
       </Mutation>
-        )
+    );
   }
 }
 
